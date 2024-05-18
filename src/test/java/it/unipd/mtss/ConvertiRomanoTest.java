3d42a2cdfa6b2bd0073512e9d965d8abd2ee1fa8
ColdBerrri <darkk.matter391@gmail.com>
2024-05-18 20:07:57 +0200

    close #3
    risolta terza issue

--- src/test/java/it/unipd/mtss/ConvertiRomanoTest.java
+++ src/test/java/it/unipd/mtss/ConvertiRomanoTest.java
@@ -8,15 +8,15 @@ public class ConvertiRomanoTest {
     //test dei numeri fuori dal range, oltre mille, zero o sotto 1
     @Test(expected = IllegalArgumentException.class)
         public void overRangeThrowsIllegalArgumentException() {
-            ConvertiRomano.convert(1111);
+    		ConvertiRomano.convert(1111);
         }
     @Test(expected = IllegalArgumentException.class)
         public void underRangeThrowsIllegalArgumentException() {
-            ConvertiRomano.convert(0);
+    		ConvertiRomano.convert(0);
         }
     @Test(expected = IllegalArgumentException.class)
         public void underZeroThrowsIllegalArgumentException() {
-            ConvertiRomano.convert(-1);
+    		ConvertiRomano.convert(-1);
         }
 
     //test primi 3 numeri
@@ -33,5 +33,19 @@ public class ConvertiRomanoTest {
         assertEquals(ConvertiRomano.convert(4), "IV");
         assertEquals(ConvertiRomano.convert(5), "V");
         assertEquals(ConvertiRomano.convert(6), "VI");
+    }
+
+    //primi 9 numeri continuo    
+    @Test
+    public void First9NumbersConversion() {
+        assertEquals(ConvertiRomano.convert(7), "VII");
+        assertEquals(ConvertiRomano.convert(8), "VIII");
+        assertEquals(ConvertiRomano.convert(9), "IX");
+    }
+
+    //numeri con una sola lettera fino a 10
+    @Test
+    public void OneLetterNumbers() {
+        assertEquals(ConvertiRomano.convert(10), "X");
     }
 }
--- src/test/java/it/unipd/mtss/StampaAsciiTest.java
+++ src/test/java/it/unipd/mtss/StampaAsciiTest.java
@@ -9,9 +9,9 @@ public class StampaAsciiTest {
     //test per caratteri che non dovrebbero comparire
     @Test(expected = IllegalArgumentException.class)
     public void characterNotPresentInRomanFormat() {
-        StampaAscii.printAsciiArt("gg");
-        StampaAscii.printAsciiArt("cane");
-        StampaAscii.printAsciiArt("ff");
+        StampaAscii.printAsciiArt("W");
+        StampaAscii.printAsciiArt("lol");
+        StampaAscii.printAsciiArt("b");
         StampaAscii.printAsciiArt(null);
     }
 
@@ -40,5 +40,18 @@ public class StampaAsciiTest {
             result.append(" \n");
         String stringa= result.toString(); 
             assertEquals(StampaAscii.printAsciiArt("V"), stringa);
+    }
+
+    @Test
+    public void TestX() {
+        StringBuilder result = new StringBuilder();
+            result.append("*    * \n");
+            result.append(" *  * \n");
+            result.append("  *   \n");
+            result.append(" *  * \n");
+            result.append("*    *\n");
+            result.append(" \n");
+        String stringa= result.toString(); 
+            assertEquals(StampaAscii.printAsciiArt("X"), stringa);
     }
 }
